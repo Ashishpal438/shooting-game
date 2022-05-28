64b51d28d825e6856680965b57ba96c252b02feb
Ashish kumar Pal <ashishpal438@gmail.com>
2022-05-29 01:12:26 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ function App() {
 
   
   const moveToBox = (index) => {
+    while(color[index] === null){
+      index++
+    }
     setDisplay([...display, {color : color[index], index : index }])
     const newarr = color.map((item, ind) => {
       if(index === ind){
@@ -36,9 +39,9 @@ function App() {
     })
 
     setColor(newarr)
+    setInput("")
   }
   
-  console.log("original array ==> ",color);
 
   const handleDisplay = (item) => {
     const newarr = color.map((ele, ind) => {
@@ -50,36 +53,40 @@ function App() {
       }
     })
     setColor(newarr)
-
-    const filtered = display.filter((ele,index) => index !== item.index)
-    setDisplay(filtered)
+    setDisplay(display.filter((ele) => ele.index !== item.index))
   }
 
-  console.log("displaybox",display);
 
   return (
     <div className="App">
-      <div className='display'>
+      <div className='display-container'>
+        { display.length > 0 ? <b>Click on the ball to move it back to it's previous position</b> : ""}
+         <div className='display'>
          {
            display && display.map((item) => {
              return <div key={uuidv4()}  className='circle' style={{ backgroundColor: `${item.color}` }} onClick={() => handleDisplay(item)}></div>
            })
          }
+         </div>
       </div>
       <div className='circle-container'>
         {
-          color && color.map((item) => {
-            return item ? <div key={uuidv4()} className='circle' style={{ backgroundColor: `${item}` }}></div> : ''
+          color && color.map((item,index) => {
+            return item ? <div key={uuidv4()} className='circle' style={{ backgroundColor: `${item}` }}> </div> : ''
           })
         }
       </div>
-      <div>
+      <div className='input-container'>
+       <div>
+         <b>Enter the serial number of the ball  you want to shoot</b>
+       </div>
         <input
           type="number"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter number"
         />
-        <button onClick={() => moveToBox(input-1)}>Shoot</button>
+        <button onClick={() => input ? moveToBox(input-1) : ""}>Shoot</button>
       </div>
     </div>
   );
